f860be1855a40670a1d5d9212367dbd7c56a8221
Daniil Kargashin <danilo98.24fevral@yandex.ru>
2025-04-13 00:48:09 +0300

    primary project file main.py

---
+++ src/main.py
@@ -0,0 +1,32 @@
+class Product:
+    """Класс, содержащий в себе информацию о товаре"""
+
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        """Метод для инициализации экземпляра класса. Задаем значение атрибутам экземпляра товара"""
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
+
+class Category:
+    """Класс, содержащий в себе информацию о категориях товара"""
+
+    name: str
+    description: str
+    products: list
+    category_count = 0
+    product_count = 0
+
+    def __init__(self, name, description, products):
+        """Метод для инициализации категории. Задаем значение атрибутам категории товара"""
+        self.name = name
+        self.description = description
+        self.products = products
+        Category.category_count += 1
+        Category.product_count += len(products)
