2956c0dfdae67a2a3fd01bf9f09cb1fe29804fc1
Daniil Kargashin <danilo98.24fevral@yandex.ru>
2025-04-13 00:48:31 +0300

    primary tests

---
+++ tests/conftest.py
@@ -0,0 +1,34 @@
+import pytest
+
+from src.main import Product, Category
+
+@pytest.fixture
+def first_prod():
+    """Фикстура для теста класса первого продукта"""
+    return Product(
+        name = "Микроволновка",
+        description = "Микроволновая печь, предназначенная для подогрева пищи",
+        price = 26000,
+        quantity = 7,
+    )
+
+
+@pytest.fixture
+def second_prod():
+    """Фикстура для теста класса второго продукта"""
+    return Product(
+        name = "Мультиварка",
+        description = "Мультиварка, предназначенная для приготовления пищи",
+        price = 23500,
+        quantity = 4,
+    )
+
+
+@pytest.fixture
+def first_category():
+    """Фикстура для теста категории продуктов"""
+    return Category(
+        name = "Телевизоры",
+        description = "Телевизоры - устройства для просмотра визуальной мультимедии",
+        products = ["Samsung", "LG"]
+    )
---
+++ tests/test_main.py
@@ -0,0 +1,35 @@
+import pytest
+
+from src.main import Product, Category
+
+def test_prod_init(first_prod, second_prod):
+    """Тест на наличие корректных атрибутов"""
+    assert first_prod.name == "Микроволновка"
+    assert first_prod.description == "Микроволновая печь, предназначенная для подогрева пищи"
+    assert first_prod.price == 26000
+    assert first_prod.quantity == 7
+
+    assert second_prod.name == "Мультиварка"
+    assert second_prod.description == "Мультиварка, предназначенная для приготовления пищи"
+
+
+def test_category_init(first_category):
+    """Тест категорий продуктов"""
+    assert first_category.name == "Телевизоры"
+    assert first_category.description == "Телевизоры - устройства для просмотра визуальной мультимедии"
+    assert first_category.products == ["Samsung", "LG"]
+
+
+def test_total_categories_counter():
+    """Тест счётчика количества категорий"""
+    Category.category_count = 0
+    category = Category("Тест", "Описание", [])
+    assert Category.category_count == 1
+
+
+def test_unique_products_counter():
+    """Тест счётчика уникальных товаров"""
+    Category.product_count = 0
+    test_product = Product("Телефон", "Смартфон", 50000, 10)
+    category = Category("Электроника", "Техника", [test_product])
+    assert Category.product_count == 1
